7bc9d71daca6e54ea3fcb9811a8fcfe008516c03
Jan <jan.reuter2909@gmail.com>
2024-04-28 20:57:38 +0200

    added day4 part 1

--- AoCPractice/2021/Day4/main.go
+++ AoCPractice/2021/Day4/main.go
@@ -19,7 +19,7 @@ var (
 
 func partOne(scanner bufio.Scanner) {
 	i := 0
-    tmp := make([]int, 0)
+	tmp := make([]int, 0)
 
 	for scanner.Scan() {
 		if i == 0 {
@@ -31,7 +31,7 @@ func partOne(scanner bufio.Scanner) {
 
 		if i > 1 {
 			if scanner.Text() != "" {
-                trimLine := strings.ReplaceAll(scanner.Text(), "  ", " ")
+				trimLine := strings.ReplaceAll(scanner.Text(), "  ", " ")
 
 				for _, e := range strings.Split(trimLine, " ") {
 					val, _ := strconv.Atoi(e)
@@ -39,95 +39,86 @@ func partOne(scanner bufio.Scanner) {
 				}
 			} else {
 				boards = append(boards, tmp)
-                tmp = make([]int, 0)
+				tmp = make([]int, 0)
 			}
 		}
 
 		i++
 	}
-    
-    boards = append(boards, tmp)
 
-    for _, n := range nums {
-        for _, b := range boards {
-            for i, e := range b {
-                if e == n {
-                    b[i] = -1
-                }
-            }
+	boards = append(boards, tmp)
 
-            if hasWon(b) {
-                tmp := 0
+	for _, n := range nums {
+		for _, b := range boards {
+			for i, e := range b {
+				if e == n {
+					b[i] = -1
+				}
+			}
 
-                for _, e := range b {
-                    if e != -1 {
-                        tmp += e
-                    }
-                }
+			if hasWon(b) {
+				tmp := 0
 
-                fmt.Printf("result: %d, %d\n", tmp, n)
-                return
-            }
-        }
-    }
+				for _, e := range b {
+					if e != -1 {
+						tmp += e
+					}
+				}
+
+				fmt.Printf("result: %d\n", tmp * n)
+				return
+			}
+		}
+	}
 }
 
 func hasWon(board []int) bool {
-    fmt.Printf("%d\n", board)
-	lb := 0
-	ub := lb + boardSize
-    tmp := make([]int, 0)
+	marks := make([]int, 0)
 
-	// horizontal check
-	for ub+boardSize <= len(board) {
-        fmt.Printf("%d\n", board[lb:ub])
-		for _, e := range board[lb:ub] {
-			if e == -1 {
-                tmp = append(tmp, e)
-			}
+	for i, e := range board {
+		if e == -1 {
+			marks = append(marks, i)
 		}
-
-		lb += boardSize
-		ub += boardSize
-
-        if len(tmp) == 5 {
-            return true
-        } else {
-            tmp = make([]int, 0)
-        }
 	}
 
-    tmp = make([]int, 0)
+	horHits := 0
+	vertHits := 0
 
-	// vertical check
-	for range boardSize {
-		var tmp []int
+	for i := range marks {
+		if i+1 < len(marks) {
+			if marks[i+1] == marks[i] + 1 && (marks[i] + 1) % boardSize != 0 {
+				horHits++
+			} else {
+                horHits = 0
+            }
 
-		val := 0
-		for range boardSize {
-			tmp = append(tmp, board[val])
-			val += boardSize
+            if horHits == boardSize - 1 {
+                return true
+            }
 		}
-
-		for _, e := range tmp {
-			if e == -1 {
-			    tmp = append(tmp, e)
-			}
-		}
-
-        if len(tmp) == 5 {
-            return true
-        } else {
-            tmp = make([]int, 0)
-        }
 	}
 
-    
-    return false
+	for i := range marks {
+		if i+1 < len(marks) {
+			if marks[i+1] == marks[i] + boardSize && (marks[i] + 1) % boardSize != 0 {
+                vertHits++
+			} else {
+                vertHits = 0
+            }
+
+            if vertHits == boardSize - 1 {
+                return true
+            }
+		}
+	}
+
+	// fmt.Printf("board: %d, marks: %d, hor: %d, vert: %d\n", board, marks, horHits, vertHits)
+
+	return false
 }
 
 func main() {
-	fi, err := os.Open("input.txt")
+	fi, err := os.Open("input1.txt")
 	if err != nil {
 		panic(err)
 	}
