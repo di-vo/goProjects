444b3aa5dd498db4d2993b3f58ef43c0a62bfde9
Jan <jan.reuter2909@gmail.com>
2024-04-29 17:03:14 +0200

    day 5 part 1

--- AoCPractice/2021/Day5/main.go
+++ AoCPractice/2021/Day5/main.go
@@ -5,18 +5,17 @@ import (
 	"fmt"
 	"os"
 	"slices"
-	"sort"
 	"strconv"
 	"strings"
 )
 
 const (
-    fieldSize = 10
+	fieldSize = 990
 )
 
 var (
 	lines []VectorLine
-    field []Vector
+	field []Vector
 )
 
 type Vector struct {
@@ -28,7 +27,7 @@ type VectorLine struct {
 }
 
 func partOne(scanner bufio.Scanner) {
-    // get lines
+	// get lines
 	for scanner.Scan() {
 		text := strings.ReplaceAll(scanner.Text(), " ", "")
 		linesText := strings.Split(text, "->")
@@ -38,50 +37,76 @@ func partOne(scanner bufio.Scanner) {
 		x2, _ := strconv.Atoi(strings.Split(linesText[1], ",")[0])
 		y2, _ := strconv.Atoi(strings.Split(linesText[1], ",")[1])
 
-		if x1 == x2 || y1 == y2 {
-			lines = append(lines, VectorLine{
-                start: Vector{x: x1, y: y1, val: 1},
-                end:   Vector{x: x2, y: y2, val: 1},
+		if x1 == x2 {
+			if y1 < y2 {
+				lines = append(lines, VectorLine{
+					start: Vector{x: x1, y: y1, val: 1},
+					end:   Vector{x: x2, y: y2, val: 1},
+				})
+			} else {
+				lines = append(lines, VectorLine{
+					start: Vector{x: x2, y: y2, val: 1},
+					end:   Vector{x: x1, y: y1, val: 1},
+				})
+            }
+		} else if y1 == y2 {
+			if x1 < x2 {
+				lines = append(lines, VectorLine{
+					start: Vector{x: x1, y: y1, val: 1},
+					end:   Vector{x: x2, y: y2, val: 1},
+				})
+			} else {
+				lines = append(lines, VectorLine{
+					start: Vector{x: x2, y: y2, val: 1},
+					end:   Vector{x: x1, y: y1, val: 1},
+				})
+            }
+        }
+	}
+
+	// get field
+	for i := 0; i < fieldSize; i++ {
+		for j := 0; j < fieldSize; j++ {
+			field = append(field, Vector{
+				x: i,
+				y: j,
 			})
 		}
 	}
 
-    sort.Slice(lines, func(i, j int) bool {
-        if lines[i].start.x == lines[j].start.x {
-            //TODO: implement sorting 
+	for _, e := range lines {
+		if e.start.x == e.end.x {
+			for i := e.start.y; i <= e.end.y; i++ {
+				idx := slices.IndexFunc(field, func(v Vector) bool { return v.x == e.start.x && v.y == i })
+
+				if idx != -1 {
+					field[idx].val++
+				}
+			}
+		} else if e.start.y == e.end.y {
+			for i := e.start.x; i <= e.end.x; i++ {
+				idx := slices.IndexFunc(field, func(v Vector) bool { return v.x == i && v.y == e.start.y })
+
+				if idx != -1 {
+					field[idx].val++
+				}
+			}
         }
-    })
+	}
 
-    // get field
-    for i := 0; i < fieldSize; i++ {
-        for j := 0; j < fieldSize; j++ {
-            field = append(field, Vector{
-                x: i,
-                y: j,
-            })
-        }
-    }
-
-    fmt.Printf("lines: %d\n", lines)
-
-    for _, e := range lines {
-        if e.start.x == e.end.x {
-            fmt.Printf("x1: %d, y1: %d, x2: %d, y2: %d\n", e.start.x, e.start.y, e.end.x, e.end.y)
-            for i := e.start.y; i <= e.end.y; i++ {
-                idx := slices.IndexFunc(field, func(v Vector) bool { return v.x == e.start.x && v.y == i }) 
-
-                if idx != -1 {
-                    field[idx].val++
-                }
-            }
+    // get result
+    result := 0
+    for _, e := range field {
+        if e.val >= 2 {
+            result++
         }
     }
 
-    fmt.Printf("field after: %d\n", field)
+    fmt.Printf("result: %d\n", result)
 }
 
 func main() {
-	fi, err := os.Open("input.txt")
+	fi, err := os.Open("input1.txt")
 	if err != nil {
 		panic(err)
 	}
