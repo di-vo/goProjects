26628a5aca928770e1eebc58069e7acaee4bd3b5
Jan Reuter <Jan.Reuter@edu.bib.de>
2024-06-21 12:33:56 +0200

    added progress bar

--- imageFilter/cmd/main.go
+++ imageFilter/cmd/main.go
@@ -11,20 +11,23 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	ansi "github.com/k0kubun/go-ansi"
+	"github.com/schollz/progressbar/v3"
 )
 
 var (
-    filterFlag  = flag.String("f", "", "The filter to apply to the image. Valid filters are:\n boxBlur\n gaussianBlur\n edge\n spot\n invert\n comic\n heat\n sort (VERY SLOW!)\n pixel (experimental)\n basicKuwahara\n generalKuwahara (experimental)")
+	filterFlag  = flag.String("f", "", "The filter to apply to the image. Valid filters are:\n boxBlur\n gaussianBlur\n edge\n spot\n invert\n comic\n heat\n sort (VERY SLOW!)\n pixel (experimental)\n basicKuwahara\n generalKuwahara (experimental)")
 	sourceFlag  = flag.String("s", "", "The name of the source image file")
 	helpFlag    = flag.Bool("h", false, "Shows this help message")
 	convertFlag = flag.Bool("c", false, "Create a new PNG from the given JPEG image")
-    threadsFlag = flag.Int("t", runtime.GOMAXPROCS(0), "Specify the number of threads used for the filter(0 < x <= image height)\nDefaults to the number of available threads")
+	threadsFlag = flag.Int("t", runtime.GOMAXPROCS(0), "Specify the number of threads used for the filter(0 < x <= image height)\nDefaults to the number of available threads")
 	outputName  string
 	filterName  string
 )
 
 func main() {
-    start := time.Now()
+	start := time.Now()
 	flag.Parse()
 
 	if *helpFlag {
@@ -51,7 +54,7 @@ func main() {
 
 	imgCopy := image.NewRGBA64(image.Rect(0, 0, imgWidth, imgHeight))
 
-    //fmt.Printf("threads: %d\n", *threadsFlag)
+	//fmt.Printf("threads: %d\n", *threadsFlag)
 	partWidth := imgWidth
 	partHeight := imgHeight / *threadsFlag
 
@@ -72,6 +75,21 @@ func main() {
 		}
 	}
 
+	bar := progressbar.NewOptions(int(*threadsFlag),
+		progressbar.OptionSetWriter(ansi.NewAnsiStdout()),
+		progressbar.OptionSetWidth(50),
+		progressbar.OptionSetElapsedTime(true),
+		progressbar.OptionSetPredictTime(true),
+		progressbar.OptionSetDescription("Applying filter..."),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "—",
+			SaucerHead:    ">",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}),
+		progressbar.OptionOnCompletion(func() { fmt.Println() }))
+
 	go func() {
 		for m := range imageChan {
 			// fmt.Printf("image received: %d, %d\n", m.StartX, m.StartY)
@@ -81,13 +99,14 @@ func main() {
 				}
 			}
 			wg.Done()
+			bar.Add(1)
 		}
 	}()
 
 	wg.Wait()
-    fmt.Printf("Filter applied.\n")
+	// fmt.Printf("Filter applied.\n")
 
 	utils.SaveNewImage(imgCopy, fmt.Sprintf("%s_%s.png", strings.Split(*sourceFlag, ".")[0], *filterFlag))
-    end := time.Since(start)
-    fmt.Printf("Done!\nExecution Time: %.4fs\n", end.Seconds())
+	end := time.Since(start)
+	fmt.Printf("Done!\nExecution Time: %.4fs\n", end.Seconds())
 }
